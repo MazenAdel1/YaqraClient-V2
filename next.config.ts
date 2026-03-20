1e081c87b81d058953042bd5e71b33049d8fed65
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-20 05:49:36 +0200

    add localhost images origin for development

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "localhost",
+        port: "7282",
+        pathname: "/**",
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
