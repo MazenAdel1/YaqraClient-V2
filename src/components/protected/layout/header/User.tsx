bc32b4d9fe35d4cd834e11f959d44df0ba58f6c1
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-21 17:45:47 +0200

    feat: User component init

---
+++ src/components/protected/layout/header/User.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useUserStore } from "@/providers/user-store-provider";
+import { UserImage } from "../../shared/User";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import Logout from "./Logout";
+import { UserIcon } from "lucide-react";
+
+export default function User() {
+  const { user } = useUserStore();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserImage innavigable />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="max-sm:w-[calc(100vw-2rem)]! max-sm:min-w-0! sm:w-50"
+        align="end"
+      >
+        <DropdownMenuGroup className="*:w-full">
+          <DropdownMenuItem
+            nativeButton={false}
+            render={
+              <Link
+                href={`/profile/${user?.id}`}
+                className="flex items-center justify-between"
+              >
+                الملف الشخصي <UserIcon />
+              </Link>
+            }
+          />
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup className="*:w-full">
+          <DropdownMenuItem render={<Logout />} />
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
