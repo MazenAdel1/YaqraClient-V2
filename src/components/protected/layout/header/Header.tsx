a98882e42bbb37e5f4a94ce8dcc09b80d8a93cd5
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-21 17:44:57 +0200

    feat: Header component init

---
+++ src/components/protected/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import Notification from "./Notification";
+import User from "./User";
+
+export default function Header() {
+  return (
+    <header className="bg-dark-gray border-light-gray fixed top-0 left-1/2 w-full -translate-x-1/2 border-b shadow-sm shadow-black">
+      <div className="container flex w-full items-center justify-between py-5">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={100} height={100} />
+        </Link>
+        <div className="flex items-center gap-3">
+          <Notification />
+          <User />
+        </div>
+      </div>
+    </header>
+  );
+}
