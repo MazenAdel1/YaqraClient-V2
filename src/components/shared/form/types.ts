1b3313f083ad7b657d5fa8d62a6d8d63e0105558
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-23 10:48:28 +0200

    add image picker props & ref for Form

--- src/components/shared/form/types.ts
+++ src/components/shared/form/types.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 import { DefaultValues, Path, SubmitHandler } from "react-hook-form";
-import { HTMLInputTypeAttribute } from "react";
+import { HTMLInputTypeAttribute, Ref } from "react";
 
 export type AnyObjectSchema = z.ZodObject<z.ZodRawShape>;
 
@@ -11,6 +11,7 @@ export type FormInput<S extends AnyObjectSchema> = {
   ariaLabel?: string;
   placeholder?: string;
   autoFocus?: boolean;
+  accept?: string;
 };
 
 export type FormProps<S extends AnyObjectSchema> = {
@@ -19,4 +20,18 @@ export type FormProps<S extends AnyObjectSchema> = {
   defaultValues?: DefaultValues<z.input<S>>;
   onSubmit: SubmitHandler<z.output<S>>;
   submitLabel?: string;
+  ref?: Ref<HTMLFormElement>;
+};
+
+export type ImagePickerProps = {
+  id: string;
+  name: string;
+  accept?: string;
+  ariaLabel?: string;
+  value?: File | string;
+  disabled?: boolean;
+  autoFocus?: boolean;
+  invalid?: boolean;
+  onBlur?: () => void;
+  onChange: (file?: File) => void;
 };
