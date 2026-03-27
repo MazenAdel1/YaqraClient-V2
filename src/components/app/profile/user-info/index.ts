c703711718be37d8f478f5a3ffa9b71023d2a587
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-27 20:23:05 +0200

    export profile client header component

--- src/components/app/profile/user-info/index.ts
+++ src/components/app/profile/user-info/index.ts
@@ -3,3 +3,4 @@ export { default as FollowDialog } from "./FollowDialog";
 export { default as PasswordEditDialog } from "./PasswordEditDialog";
 export { default as ProfileEditDialog } from "./ProfileEditDialog";
 export { default as UserInfo } from "./UserInfo";
+export { default as ProfileHeaderClient } from "./ProfileHeaderClient";
