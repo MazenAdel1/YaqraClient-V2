353764d0a1f18270a4501d279589e0692e00818d
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-29 20:32:26 +0200

    goal component exports index init

---
+++ src/components/app/profile/body/tabs/goals/index.ts
@@ -0,0 +1,5 @@
+export { type GoalProps, type GoalDialogProps } from "./types";
+export { default as Goal } from "./Goal";
+export { default as GoalDialog } from "./GoalDialog";
+export { default as DeleteGoal } from "./DeleteGoal";
+export { default as Goals } from "./Goals";
