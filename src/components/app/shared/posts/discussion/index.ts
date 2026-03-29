1b36e544867e0bbc58325fc74b8dc5d9d4cd1816
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-29 18:02:32 +0200

    discussion exports index init

---
+++ src/components/app/shared/posts/discussion/index.ts
@@ -0,0 +1,3 @@
+export { type DiscussionProps, type DiscussionDialogProps } from "./types";
+export { default as Discussion } from "./Discussion";
+export { default as DiscussionDialog } from "./DiscussionDialog";
