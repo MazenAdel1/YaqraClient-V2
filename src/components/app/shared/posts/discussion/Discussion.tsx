1df2c0f0ff474bdae040c41c4a18938ed5a64c1b
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-30 21:39:54 +0200

    wrap posts with new actions wrapper

--- src/components/app/shared/posts/discussion/Discussion.tsx
+++ src/components/app/shared/posts/discussion/Discussion.tsx
@@ -16,6 +16,7 @@ import DeletePost from "../shared/DeletePost";
 import { Badge } from "@/components/ui/badge";
 import { TAGS } from "./consts";
 import FormattedText from "../../FormattedText";
+import PostActionWrapper from "../post-actions/PostActionWrapper";
 
 export default function Discussion({
   discussion,
@@ -26,56 +27,64 @@ export default function Discussion({
 
   return (
     <Card>
-      <CardHeader className="gap-4">
-        <div className="flex items-center gap-2">
-          <UserImage id={discussion.userId} />
-          <div className="flex flex-col gap-0.5">
-            <Link href={`/profile/${discussion.userId}`}>
-              {discussion.username}
-            </Link>
-            <span className="text-[10px] text-gray-300">
-              {timeAgo(discussion.createdDate)}
-            </span>
+      <PostActionWrapper
+        postId={discussion.id}
+        isLiked={discussion.isLiked}
+        likeCount={discussion.likeCount}
+      >
+        <CardHeader className="gap-4">
+          <div className="flex items-center gap-2">
+            <UserImage id={discussion.userId} />
+            <div className="flex flex-col gap-0.5">
+              <Link href={`/profile/${discussion.userId}`}>
+                {discussion.username}
+              </Link>
+              <span className="text-[10px] text-gray-300">
+                {timeAgo(discussion.createdDate)}
+              </span>
+            </div>
           </div>
-        </div>
-        <Badge variant={"secondary"}>
-          {TAGS[discussion.tag as keyof typeof TAGS].label}
-        </Badge>
-        <div className="flex flex-col gap-2">
-          <CardTitle className="font-yoc text-xl">{discussion.title}</CardTitle>
-          <div className="font-yoc flex gap-1">
-            {discussion.books.slice(0, 3).map((book) => (
-              <div
-                key={book.id}
-                className="bg-dark-gray flex items-center gap-2 rounded-md p-1.5 text-gray-200"
-              >
-                <Book className="size-4" />
-                {book.title}
-              </div>
-            ))}
-            {discussion.books.length > 3 && (
-              <div className="bg-dark-gray flex h-20 w-15 items-center justify-center rounded-sm text-sm text-gray-300">
-                +{discussion.books.length - 3}
-              </div>
-            )}
+          <Badge variant={"secondary"}>
+            {TAGS[discussion.tag as keyof typeof TAGS].label}
+          </Badge>
+          <div className="flex flex-col gap-2">
+            <CardTitle className="font-yoc text-xl">
+              {discussion.title}
+            </CardTitle>
+            <div className="font-yoc flex gap-1">
+              {discussion.books.slice(0, 3).map((book) => (
+                <div
+                  key={book.id}
+                  className="bg-dark-gray flex items-center gap-2 rounded-md p-1.5 text-gray-200"
+                >
+                  <Book className="size-4" />
+                  {book.title}
+                </div>
+              ))}
+              {discussion.books.length > 3 && (
+                <div className="bg-dark-gray flex h-20 w-15 items-center justify-center rounded-sm text-sm text-gray-300">
+                  +{discussion.books.length - 3}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-        {theCurrentUser?.id === discussion.userId && (
-          <CardAction className="border-light-gray flex gap-1 rounded-md border">
-            <DiscussionDialog type="edit" discussion={discussion} />
-            <DeletePost
-              postId={discussion.id}
-              queryKey="profile-discussions"
-              title="هل أنت متأكد من حذف هذا النقاش؟"
-            />
-          </CardAction>
+          {theCurrentUser?.id === discussion.userId && (
+            <CardAction className="border-light-gray flex gap-1 rounded-md border">
+              <DiscussionDialog type="edit" discussion={discussion} />
+              <DeletePost
+                postId={discussion.id}
+                queryKey="profile-discussions"
+                title="هل أنت متأكد من حذف هذا النقاش؟"
+              />
+            </CardAction>
+          )}
+        </CardHeader>
+        {discussion.content && (
+          <CardContent>
+            <FormattedText text={discussion.content} />
+          </CardContent>
         )}
-      </CardHeader>
-      {discussion.content && (
-        <CardContent>
-          <FormattedText text={discussion.content} />
-        </CardContent>
-      )}
+      </PostActionWrapper>
     </Card>
   );
 }
--- src/components/app/shared/posts/review/Review.tsx
+++ src/components/app/shared/posts/review/Review.tsx
@@ -17,60 +17,67 @@ import Image from "next/image";
 import { Book } from "lucide-react";
 import DeletePost from "../shared/DeletePost";
 import FormattedText from "../../FormattedText";
+import PostActionWrapper from "../post-actions/PostActionWrapper";
 
 export default function Review({ review }: { review: ReviewProps }) {
   const { user: theCurrentUser } = useUserStore();
 
   return (
     <Card>
-      <CardHeader className="gap-4">
-        <div className="flex items-center gap-2">
-          <UserImage id={review.userId} />
-          <div className="flex flex-col gap-0.5">
-            <Link href={`/profile/${review.userId}`}>{review.username}</Link>
-            <span className="text-[10px] text-gray-300">
-              {timeAgo(review.createdDate)}
-            </span>
+      <PostActionWrapper
+        postId={review.id}
+        likeCount={review.likeCount}
+        isLiked={review.isLiked}
+      >
+        <CardHeader className="gap-4">
+          <div className="flex items-center gap-2">
+            <UserImage id={review.userId} />
+            <div className="flex flex-col gap-0.5">
+              <Link href={`/profile/${review.userId}`}>{review.username}</Link>
+              <span className="text-[10px] text-gray-300">
+                {timeAgo(review.createdDate)}
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center gap-2">
-          <div className="bg-dark-gray flex h-20 w-15 items-center justify-center overflow-hidden rounded-sm">
-            {review.book.image ? (
-              <Image
-                src={`${process.env.NEXT_PUBLIC_SERVER_URL}${review.book.image}`}
-                alt={review.book.title}
-                width={50}
-                height={74}
-                className="size-full"
+          <div className="flex items-center gap-2">
+            <div className="bg-dark-gray flex h-20 w-15 items-center justify-center overflow-hidden rounded-sm">
+              {review.book.image ? (
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_SERVER_URL}${review.book.image}`}
+                  alt={review.book.title}
+                  width={50}
+                  height={74}
+                  className="size-full"
+                />
+              ) : (
+                <Book className="size-6" />
+              )}
+            </div>
+            <div className="flex flex-col gap-0.5">
+              <CardTitle className="text-lg">{review.title}</CardTitle>
+              <h2 className="flex items-center gap-2">
+                <span className="font-yoc text-lg font-medium text-gray-300">
+                  {review.book.title}
+                </span>{" "}
+                <Rate rate={review.rate} />
+              </h2>
+            </div>
+          </div>
+          {theCurrentUser?.id === review.userId && (
+            <CardAction className="border-light-gray flex gap-1 rounded-md border">
+              <ReviewDialog type="edit" review={review} />
+              <DeletePost
+                postId={review.id}
+                queryKey="profile-reviews"
+                title="هل أنت متأكد من حذف هذه المراجعة؟"
               />
-            ) : (
-              <Book className="size-6" />
-            )}
-          </div>
-          <div className="flex flex-col gap-0.5">
-            <CardTitle className="text-lg">{review.title}</CardTitle>
-            <h2 className="flex items-center gap-2">
-              <span className="font-yoc text-lg font-medium text-gray-300">
-                {review.book.title}
-              </span>{" "}
-              <Rate rate={review.rate} />
-            </h2>
-          </div>
-        </div>
-        {theCurrentUser?.id === review.userId && (
-          <CardAction className="border-light-gray flex gap-1 rounded-md border">
-            <ReviewDialog type="edit" review={review} />
-            <DeletePost
-              postId={review.id}
-              queryKey="profile-reviews"
-              title="هل أنت متأكد من حذف هذه المراجعة؟"
-            />
-          </CardAction>
-        )}
-      </CardHeader>
-      <CardContent>
-        <FormattedText text={review.content} />
-      </CardContent>
+            </CardAction>
+          )}
+        </CardHeader>
+        <CardContent>
+          <FormattedText text={review.content} />
+        </CardContent>
+      </PostActionWrapper>
     </Card>
   );
 }
