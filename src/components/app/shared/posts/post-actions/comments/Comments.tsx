772c86d342aef0b3478c4a369d8ad96a4b8e095e
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-01 20:45:10 +0200

    comments parent component init

--- src/components/app/shared/posts/post-actions/comments/Comments.tsx
+++ src/components/app/shared/posts/post-actions/comments/Comments.tsx
@@ -1,29 +1,33 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { MessagesSquare } from "lucide-react";
-import { CommentsProps } from "./types";
+import { axios } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import { CommentProps } from "./types";
+import Comment from "./Comment";
+import { Loader2 } from "lucide-react";
 
-export default function Comments({ children }: CommentsProps) {
+export default function Comments({ postId }: { postId: number }) {
+  const { data: comments, isLoading } = useQuery<CommentProps[]>({
+    queryKey: ["comments", postId],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `/community/postComments?postId=${postId}`,
+      );
+      return data.result.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loader2 className="mx-auto size-7 animate-spin" />;
+  }
+
   return (
-    <Dialog>
-      <DialogTrigger
-        render={
-          <Button variant={"outline"}>
-            <MessagesSquare className="size-4" />
-          </Button>
-        }
-      />
-      <DialogContent
-        render={
-          <Card className="flex flex-col gap-2">
-            <Card>{children}</Card>
-            <Card>
-              <CardContent>comment</CardContent>
-            </Card>
-          </Card>
-        }
-      />
-    </Dialog>
+    <section className="flex flex-col gap-2">
+      {comments?.map((comment) => (
+        <div key={comment.id} className="flex flex-col gap-1">
+          {!comment.parentCommentId && (
+            <Comment key={comment.id} comment={comment} />
+          )}
+        </div>
+      ))}
+    </section>
   );
 }
