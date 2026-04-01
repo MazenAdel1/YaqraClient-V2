0cf05a8de6a3461aacaf771c4873898013405b5b
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-01 20:47:00 +0200

    comment component init

---
+++ src/components/app/shared/posts/post-actions/comments/Comment.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/utils";
+import { CommentProps } from "./types";
+import { UserImage } from "@/components/app/shared/user";
+import FormattedText from "@/components/app/shared/FormattedText";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import CommentActions from "./comment-actions/CommentActions";
+import { useUserStore } from "@/providers/user-store-provider";
+import CommentModifiers from "./comment-modifiers/CommentModifiers";
+
+export default function Comment({
+  comment,
+  isReply,
+}: {
+  comment: CommentProps;
+  isReply?: boolean;
+}) {
+  const { user: theCurrentUser } = useUserStore();
+
+  return (
+    <>
+      <div className={cn(isReply && "ms-6")}>
+        <Card key={comment.id} className="gap-2 px-2 py-3">
+          <CardHeader className="px-1">
+            <CardTitle>
+              <UserImage id={comment.user.userId} user={comment.user} />
+            </CardTitle>
+            {comment.user.userId === theCurrentUser?.id && (
+              <CardAction>
+                <CommentModifiers comment={comment} />
+              </CardAction>
+            )}
+          </CardHeader>
+          <CardContent className="px-2">
+            <FormattedText text={comment.content} />
+          </CardContent>
+        </Card>
+        <CommentActions comment={comment} />
+      </div>
+      {comment.replies?.map((reply) => (
+        <Comment key={reply.id} comment={reply} isReply />
+      ))}
+    </>
+  );
+}
