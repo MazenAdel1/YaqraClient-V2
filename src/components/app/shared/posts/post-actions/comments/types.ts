7ff47f0ffe42b361eaf955249dc4eacd3fc62f85
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-01 20:45:21 +0200

    comments types init

--- src/components/app/shared/posts/post-actions/comments/types.ts
+++ src/components/app/shared/posts/post-actions/comments/types.ts
@@ -1,3 +1,19 @@
-export type CommentsProps = {
+export type CommentsDialogProps = {
   children: React.ReactNode;
+  postId: number;
+};
+
+export type CommentProps = {
+  id: number;
+  content: string;
+  parentCommentId: number | null;
+  replies: CommentProps[];
+  likeCount: number;
+  isLiked: boolean;
+  postId: number;
+  user: {
+    userId: string;
+    username: string;
+    picture: string | null;
+  };
 };
