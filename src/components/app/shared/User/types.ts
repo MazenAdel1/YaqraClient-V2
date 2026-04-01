1c120f066501c60b3e304c159feb413059b5734c
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-01 20:43:35 +0200

    update user image props

--- src/components/app/shared/User/types.ts
+++ src/components/app/shared/User/types.ts
@@ -1,4 +1,8 @@
+import { CommentProps } from "../posts/post-actions/comments/types";
+
 export type UserImageProps = {
   id: string;
+  user?: CommentProps["user"];
   innavigable?: boolean;
+  showName?: boolean;
 };
