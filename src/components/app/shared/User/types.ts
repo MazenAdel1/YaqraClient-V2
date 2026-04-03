b93083e595f6b170cb4ba499a875e8e5329922b4
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-03 23:16:22 +0200

    udpate UserImage props

--- src/components/app/shared/User/types.ts
+++ src/components/app/shared/User/types.ts
@@ -1,8 +1,8 @@
-import { CommentProps } from "../posts/post-actions/comments/types";
+import { CommentProps } from "../posts/shared/post-actions/comments/types";
 
 export type UserImageProps = {
   id: string;
   user?: CommentProps["user"];
   innavigable?: boolean;
-  showName?: boolean;
+  additionalInfo?: string;
 };
