69f4e6348abdc275823a5af506b926c97c32327e
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-08 23:16:21 +0200

    rename love to like

--- src/components/app/shared/posts/shared/post-actions/PostActionsWrapper.tsx
+++ src/components/app/shared/posts/shared/post-actions/PostActionsWrapper.tsx
@@ -1,5 +1,5 @@
 import CommentsDialog from "./comments/CommentsDialog";
-import Love from "./like/Like";
+import Like from "./like/Like";
 import { CardFooter } from "@/components/ui/card";
 import { PostActionsProps } from "./types";
 
@@ -14,7 +14,7 @@ export default function PostActionsWrapper({
       {children}
       <CardFooter>
         <div className="flex w-full items-center gap-4 *:flex-1">
-          <Love postId={postId} likeCount={likeCount} isLiked={isLiked} />
+          <Like postId={postId} likeCount={likeCount} isLiked={isLiked} />
           <CommentsDialog postId={postId}>{children}</CommentsDialog>
         </div>
       </CardFooter>
