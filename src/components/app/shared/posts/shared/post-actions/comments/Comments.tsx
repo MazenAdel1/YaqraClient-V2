9c283c96c35cedbf559455fda05ad791a8b7c9d5
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-16 00:00:38 +0200

    handle infinite scrolling in comments

--- src/components/app/shared/posts/shared/post-actions/comments/Comments.tsx
+++ src/components/app/shared/posts/shared/post-actions/comments/Comments.tsx
@@ -1,20 +1,44 @@
 import { axios } from "@/lib/axios";
-import { useQuery } from "@tanstack/react-query";
 import { CommentProps } from "./types";
 import Comment from "./Comment";
 import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { InfiniteQueryResponse } from "@/components/shared";
+import { useMemo } from "react";
 
 export default function Comments({ postId }: { postId: number }) {
-  const { data: comments, isLoading } = useQuery<CommentProps[]>({
+  const {
+    data: comments,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery<InfiniteQueryResponse<CommentProps>>({
     queryKey: ["comments", postId],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       const { data } = await axios.get(
         `/community/postComments?postId=${postId}`,
+        {
+          params: {
+            page: pageParam,
+          },
+        },
       );
 
-      return data.result.data;
+      return data.result;
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      const hasMore = lastPage.pageNumber < lastPage.totalPages;
+      return hasMore ? lastPage.pageNumber + 1 : null;
     },
   });
+
+  const commentsData = useMemo(
+    () => comments?.pages.flatMap((page) => page.data) ?? [],
+    [comments],
+  );
 
   if (isLoading) {
     return <Loader2 className="mx-auto size-7 animate-spin" />;
@@ -22,8 +46,8 @@ export default function Comments({ postId }: { postId: number }) {
 
   return (
     <section className="flex flex-col gap-2">
-      {comments && comments.length > 0 ? (
-        comments?.map((comment) => (
+      {commentsData.length > 0 ? (
+        commentsData.map((comment) => (
           <div key={comment.id} className="flex flex-col gap-1">
             {!comment.parentCommentId && (
               <Comment key={comment.id} comment={comment} />
@@ -32,6 +56,19 @@ export default function Comments({ postId }: { postId: number }) {
         ))
       ) : (
         <p className="text-center text-gray-300">لا توجد تعليقات بعد</p>
+      )}
+      {hasNextPage && (
+        <Button
+          variant="outline"
+          className={"mx-auto w-fit"}
+          disabled={isFetchingNextPage}
+          onClick={() => fetchNextPage()}
+        >
+          عرض المزيد{" "}
+          {isFetchingNextPage && (
+            <Loader2 className="mx-auto size-4 animate-spin" />
+          )}
+        </Button>
       )}
     </section>
   );
