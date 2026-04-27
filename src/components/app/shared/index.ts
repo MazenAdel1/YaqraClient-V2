37b4f4e6596bc3075e59f2ca175f3f09a046ed14
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-27 03:22:18 +0300

    export the new api prop

--- src/components/app/shared/index.ts
+++ src/components/app/shared/index.ts
@@ -1,3 +1,3 @@
 export { default as FormattedText } from "./FormattedText";
-export { type BookProps } from "./types";
+export { type BookProps, type ApiBookProps } from "./types";
 export { formatDate } from "./utils";
