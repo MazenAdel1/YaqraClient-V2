8cf6f88b134d261c7a6aa4acec6a0e376eb922ae
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-27 03:22:05 +0300

    make header at the top

--- src/components/app/layout/header/Header.tsx
+++ src/components/app/layout/header/Header.tsx
@@ -6,7 +6,7 @@ import MobileMenu from "./MobileMenu";
 
 export default function Header() {
   return (
-    <header className="bg-dark-gray border-light-gray fixed top-0 left-1/2 w-full -translate-x-1/2 border-b shadow-sm shadow-black">
+    <header className="bg-dark-gray border-light-gray fixed top-0 left-1/2 z-40 w-full -translate-x-1/2 border-b shadow-sm shadow-black">
       <div className="container flex w-full items-center justify-between py-5">
         <Link href="/">
           <Image src="/logo.svg" alt="logo" width={100} height={40} />
