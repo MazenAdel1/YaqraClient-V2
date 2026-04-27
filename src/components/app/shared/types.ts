5bc4774105975382a1b2ccb5cf37be653c1729f2
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-27 03:21:49 +0300

    add type for api and type for client

--- src/components/app/shared/types.ts
+++ src/components/app/shared/types.ts
@@ -1,6 +1,37 @@
+export type ApiBookProps = {
+  id: number;
+  title: string;
+  image?: string | null;
+  rate?: number;
+  numberOfPages?: number;
+  authorsDto: {
+    id: number;
+    name: string;
+    picture?: string;
+    bio: string;
+    rate: string;
+  }[];
+  genresDto: {
+    genreId: number;
+    genreName: string;
+  }[];
+};
+
 export type BookProps = {
   id: number;
   title: string;
   image?: string | null;
   rate?: number;
+  numberOfPages?: number;
+  author?: {
+    id: number;
+    name: string;
+    picture?: string;
+    bio: string;
+    rate: string;
+  };
+  genres?: {
+    genreId: number;
+    genreName: string;
+  }[];
 };
