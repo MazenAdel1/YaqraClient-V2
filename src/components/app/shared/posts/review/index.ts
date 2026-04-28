f90fe1fb9e496a8cd8159a53598f47d6bdf5cf1c
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-28 23:22:37 +0300

    add new export member

--- src/components/app/shared/posts/review/index.ts
+++ src/components/app/shared/posts/review/index.ts
@@ -1,3 +1,4 @@
 export { default as Review } from "./Review";
 export { default as ReviewDialog } from "./ReviewDialog";
+export { default as Rate } from "./Rate";
 export { type ReviewProps, type ReviewDialogProps } from "./types";
