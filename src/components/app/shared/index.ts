bb664ed0d84dee729b0270be7df25dffe13fc599
MazenAdel1 <mazenadel.dev@gmail.com>
2026-04-28 23:18:52 +0300

    add export index for BookCard in shared root

--- src/components/app/shared/index.ts
+++ src/components/app/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as FormattedText } from "./FormattedText";
 export { type BookProps, type ApiBookProps } from "./types";
 export { formatDate } from "./utils";
+export { default as BookCard } from "./book-card/BookCard";
