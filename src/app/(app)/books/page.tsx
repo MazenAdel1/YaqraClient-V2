1b451117addb4fa261fa4d343b6464fd7404b582
MazenAdel1 <mazenadel.dev@gmail.com>
2026-05-20 22:41:38 +0300

    check page type and add back button

--- src/app/(app)/books/page.tsx
+++ src/app/(app)/books/page.tsx
@@ -3,6 +3,9 @@ import { BookFinderPagination } from "@/components/app/book-finder";
 import { InfiniteQueryResponse } from "@/components/shared";
 import { axios } from "@/lib/axios";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 type BookType = "trending" | "recommendations" | "recent" | "upcoming";
 
@@ -18,6 +21,16 @@ export default async function page({
 
   if (page && (Number(page) < 1 || !Number.isInteger(Number(page))))
     return redirect(`/books?type=${type}`);
+
+  if (
+    !searchParams ||
+    !type ||
+    (type !== "recent" &&
+      type !== "trending" &&
+      type !== "recommendations" &&
+      type !== "upcoming")
+  )
+    return redirect(`/books?type=recommendations`);
 
   const { data } = await axios.get(`/book/${type}`, {
     params: page ? { page } : undefined,
@@ -48,7 +61,19 @@ export default async function page({
           ))}
         </div>
       ) : (
-        <p>لا يوجد كتب حاليا</p>
+        <div className="flex flex-col gap-3">
+          <Button
+            nativeButton={false}
+            variant={"secondary"}
+            className={"w-fit"}
+            render={
+              <Link href={"/feed"}>
+                <ArrowRight /> الرجوع
+              </Link>
+            }
+          />
+          <h4 className="text-xl">لا يوجد كتب حاليا</h4>
+        </div>
       )}
     </main>
   );
