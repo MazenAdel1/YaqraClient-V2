0781528834bff6e1c883e2c75877f60183cf9d48
MazenAdel1 <mazenadel.dev@gmail.com>
2026-05-21 23:09:04 +0300

    add Feed component to export index

--- src/components/app/feed/index.ts
+++ src/components/app/feed/index.ts
@@ -1,3 +1,4 @@
 export { default as SuggestedBooks } from "./SuggestedBooks";
 export { type TimelinePostProps } from "./types";
 export { default as Aside } from "./aside/Aside";
+export { default as Feed } from "./Feed";
